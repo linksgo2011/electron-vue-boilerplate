ea3fdc60815f9a81f78596fb465a98d1dd948bb9
Lin Ning <nlin@thoughtworks.com>
2019-08-07 12:18:51 +0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Version />
+  </div>
+</template>
+
+<script>
+import Version from "@/components/Version.vue";
+
+export default {
+  name: "app",
+  components: {
+    Version
+  }
+};
+</script>
